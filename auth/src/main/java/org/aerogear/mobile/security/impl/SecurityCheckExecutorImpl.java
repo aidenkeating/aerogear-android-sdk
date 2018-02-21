9aa1bcb3970f0c3cc0be9acf210c616f10a35395
Aiden Keating <akeating@redhat.com>
2018-02-21 12:00:42 +0000

    AEROGEAR-2174 Enable metrics to be sent

--- auth/src/main/java/org/aerogear/mobile/security/impl/SecurityCheckExecutorImpl.java
+++ auth/src/main/java/org/aerogear/mobile/security/impl/SecurityCheckExecutorImpl.java
@@ -2,16 +2,20 @@ package org.aerogear.mobile.security.impl;
 
 import android.content.Context;
 
+import org.aerogear.mobile.core.metrics.MetricsService;
 import org.aerogear.mobile.security.Check;
 import org.aerogear.mobile.security.SecurityCheck;
 import org.aerogear.mobile.security.SecurityCheckExecutor;
 import org.aerogear.mobile.security.SecurityCheckResult;
+import org.aerogear.mobile.security.metrics.SecurityCheckResultMetric;
 
 import java.util.ArrayList;
 
 public class SecurityCheckExecutorImpl implements SecurityCheckExecutor {
     private final ArrayList<SecurityCheck> checks;
     private final Context context;
+
+    private MetricsService metricsService;
 
     public SecurityCheckExecutorImpl(Context context) {
         this.checks = new ArrayList<>();
@@ -25,11 +29,37 @@ public class SecurityCheckExecutorImpl implements SecurityCheckExecutor {
     }
 
     @Override
+    public SecurityCheckExecutor sendMetrics(MetricsService metricsService) {
+        this.metricsService = metricsService;
+        return this;
+    }
+
+    @Override
     public SecurityCheckResult[] execute() {
+        SecurityCheckResult[] results = getTestResults();
+        if (metricsService != null) {
+            publishResultMetrics(results);
+        }
         return getTestResults();
     }
 
     /**
+     * Publish each {@link SecurityCheckResult result} provided as an {@link SecurityCheckResultMetric}.
+     *
+     * @param results Array of results
+     */
+    private void publishResultMetrics(SecurityCheckResult[] results) {
+        for(SecurityCheckResult result : results) {
+            this.metricsService.publish(new SecurityCheckResultMetric(result));
+        }
+    }
+
+    /**
+     * Return the {@link SecurityCheckResult results} for each of the tests added.
+     *
+     * @return Array of results.
+     */
+    private SecurityCheckResult[] getTestResults() {
         ArrayList<SecurityCheckResult> results = new ArrayList<>();
         for (SecurityCheck check : checks) {
             results.add(check.test(context));

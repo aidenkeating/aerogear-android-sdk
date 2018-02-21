40b7170ff6a73cc4929c885833ec875dd03b8d29
Aiden Keating <akeating@redhat.com>
2018-02-21 12:00:42 +0000

    AEROGEAR-2174 Enable metrics to be sent

--- auth/src/main/java/org/aerogear/mobile/security/Check.java
+++ auth/src/main/java/org/aerogear/mobile/security/Check.java
@@ -6,10 +6,13 @@ public enum Check {
     /**
      *  Detect whether the device is rooted.
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
      *
      * @return <code>true</code> if the device is rooted.
 >>>>>>> AEROGEAR-2174 Initial interface and class implementation
+=======
+>>>>>>> AEROGEAR-2174 Enable metrics to be sent
      */
     IS_ROOTED(new RootedCheck());
 
